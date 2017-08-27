20088532312e0aa155bae8e3f3518d4ebfff6db0
HengfengLi <hengfeng12345@gmail.com>
2017-08-27 11:24:51 +1000

    Added endpoints for reading and writing json files.

--- nginx-uwsgi-flask/app/app.py
+++ nginx-uwsgi-flask/app/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+import os
+from flask import Flask, abort, request, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    return "Hello world!"
+
+
+@app.route("/read")
+def read():
+    with open(
+            os.path.join(os.path.dirname(__file__), '../test.json')) as reader:
+        json_text = reader.read()
+        return json_text
+
+    abort(500)
+
+
+@app.route("/write/<filename>", methods=['POST'])
+def write(filename):
+    with open(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../data/{filename}.json'.format(filename=filename)),
+            'w') as writer:
+        writer.write(request.data.decode('utf-8'))
+
+    # return jsonify(success=True)
+    return ''
